bfe2854ba70189e0dc4ca9152693835bcdf8fd23
barduinor <barduinor@gmail.com>
2022-11-15 12:50:11 -0500

    added cleaner example for annotations using import

--- static/annotations/preview_annotations.js
+++ static/annotations/preview_annotations.js
@@ -1,61 +1,64 @@
 import boxAnnotations from 'https://cdn.skypack.dev/box-annotations';
 
- function preview_annotations(file_id, access_token, options){
+function preview_annotations(file_id, access_token, options) {
     var annotations = new BoxAnnotations();
     var preview = new Box.ContentPreview();
     options['boxAnnotations'] = annotations;
     return preview;
- }
- 
- var contentPreviewer = preview_annotations(file_id, accessToken, options);
- console.log('Content Previewer:', contentPreviewer);
+}
 
- contentPreviewer.addListener('viewer', previewerViewer);
-    contentPreviewer.addListener('load', previewerLoad);
-    contentPreviewer.addListener('navigate', previewerNavigate);
-    contentPreviewer.addListener('notification', previewerNotification);
-    contentPreviewer.addListener('viewerevent', previewerViewerEvent);
+var contentPreviewer = preview_annotations(file_id, accessToken, options);
+console.log('Content Previewer:', contentPreviewer);
+console.log('Content Component:', contentPreviewer.component);
+// console.log('Content Viewer:', contentPreviewer.preview.viewer);
+// console.log('Content Annotator:', contentPreviewer.preview.viewer.annotator);
 
-    // TODO: implement annotator related events
+contentPreviewer.addListener('viewer', previewerViewer);
+contentPreviewer.addListener('load', previewerLoad);
+contentPreviewer.addListener('navigate', previewerNavigate);
+contentPreviewer.addListener('notification', previewerNotification);
+contentPreviewer.addListener('viewerevent', previewerViewerEvent);
 
-    function previewerViewer(viewer) {
-        console.log('viewer object:', viewer);
-    }
+// TODO: implement annotator related events
 
-    function previewerLoad(e) {
-        console.log('load:', e);
-        sendEventToServer('load', e.file);
-    }
+function previewerViewer(viewer) {
+    console.log('viewer object:', viewer);
+}
 
-    function previewerNavigate(e) {
-        console.log('navigate:', e);
-        sendEventToServer('navigate', e);
-    }
+function previewerLoad(e) {
+    console.log('load:', e);
+    sendEventToServer('load', e.file);
+}
 
-    function previewerNotification(e) {
-        console.log('notification:', e);
-        sendEventToServer('notification', e);
-    }
+function previewerNavigate(e) {
+    console.log('navigate:', e);
+    sendEventToServer('navigate', e);
+}
 
-    function previewerViewerEvent(e) {
-        console.log('viewer event:', e);
-        sendEventToServer('viewerevent', e);
-    }
+function previewerNotification(e) {
+    console.log('notification:', e);
+    sendEventToServer('notification', e);
+}
+
+function previewerViewerEvent(e) {
+    console.log('viewer event:', e);
+    sendEventToServer('viewerevent', e);
+}
 
 
-    function sendEventToServer(eventType, e) {
+function sendEventToServer(eventType, e) {
 
-        var localData = {
-            eventType: eventType,
-            e: e
-        };
-
-        $.ajax({
-            url: "{{ url_for('event') }}",
-            type: 'POST',
-            data: JSON.stringify(localData),
-            contentType: 'application/json;charset=UTF-8',
-        });
+    var localData = {
+        eventType: eventType,
+        e: e
     };
 
-    contentPreviewer.show(file_id, accessToken, options);
+    $.ajax({
+        url: "{{ url_for('event') }}",
+        type: 'POST',
+        data: JSON.stringify(localData),
+        contentType: 'application/json;charset=UTF-8',
+    });
+};
+
+contentPreviewer.show(file_id, accessToken, options);
